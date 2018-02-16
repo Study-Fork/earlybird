ce339b7d5f48f17757b85449ba5f56000af9746d
stunstunstun <wjdsupj@gmail.com>
2018-02-16 21:05:27 +0900

    Add domain package

---
+++ packages/earlybird-domain/index.js
@@ -0,0 +1,9 @@
+export const Status = Object.freeze({
+  READY: '오늘의 일과 시작!',
+  STARTED: '오늘은 이 정도면 충분',
+  FINISHED: 'Have a good day :D',
+})
+
+const appName = 'earlybird'
+
+export default appName
